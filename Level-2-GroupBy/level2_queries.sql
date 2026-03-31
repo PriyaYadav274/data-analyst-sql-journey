450fd658f37b367cc79bc971e7805e6e2355d5e0
Priya Yadav <priyadav274@gmail.com>
2026-03-31 20:53:14 +0530

    Create level2_queries.sql for sales aggregation
    Add SQL queries for grouping and aggregating sales data.

---
+++ Level-2-GroupBy/level2_queries.sql
@@ -0,0 +1,39 @@
+USE company;
+
+-- PART 1: GROUP BY + AGGREGATES (15 Q)
+-- 1. Find total sales (amount) per customer
+SELECT 
+    customer_id, SUM(amount)
+FROM
+    Orders
+GROUP BY customer_id;
+
+-- 2. Find total number of orders per customer
+SELECT 
+    customer_id, COUNT(*)
+FROM
+    Orders 
+GROUP BY customer_id;
+
+-- 3. Find average order amount per customer
+SELECT 
+    customer_id, AVG(amount)
+FROM
+    Orders
+GROUP BY customer_id;
+
+-- 4. Find total sales per city
+SELECT 
+    c.city, SUM(o.amount)
+FROM
+    Customers AS c
+        INNER JOIN
+    Orders AS o ON c.customer_id = o.customer_id
+GROUP BY c.city;
+
+-- 5. Find highest order amount per customer
+SELECT 
+    customer_id, MAX(amount)
+FROM
+    Orders
+GROUP BY customer_id;
